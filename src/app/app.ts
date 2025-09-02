94ddb6ae8cc10837d13ce5935009d30720ba9643
Dave Lee <gdLeeToday@outlook.com>
2025-09-02 12:57:02 +0900

    layout update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { SearchForm } from './search-form/search-form';
+import { ResultList } from './result-list/result-list';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SearchForm],
-  template: `<app-search-form />`
+  imports: [SearchForm, ResultList],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
 })
-export class App {}
+export class App {
+  results: any[] = [];
+
+  // You may need to handle search event and pass results to result-list
+}
--- src/app/search-form/search-form.ts
+++ src/app/search-form/search-form.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ResultList } from '../result-list/result-list';
 import { HttpClient } from '@angular/common/http';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
@@ -12,7 +11,7 @@ import { MatTableModule } from '@angular/material/table';
 @Component({
   standalone: true,
   selector: 'app-search-form',
-  imports: [CommonModule, FormsModule, ResultList, MatInputModule, MatRadioModule, MatButtonModule, MatCardModule, MatTableModule],
+  imports: [CommonModule, FormsModule, MatInputModule, MatRadioModule, MatButtonModule, MatCardModule, MatTableModule],
   templateUrl: './search-form.html',
   styleUrls: ['./search-form.css']
 })
@@ -21,17 +20,20 @@ export class SearchForm {
   destination: string = '';
   volume: string = 'FCL';
   results: any[] = [];
+  @Output() searchResults = new EventEmitter<any[]>();
 
   constructor(private http: HttpClient) {}
 
   onSearch() {
     this.http.get<any[]>('https://rob0face.github.io/HAEZIMVO/HAEZIMVO.json')
       .subscribe(data => {
-        this.results = data.filter(item =>
+        const filtered = data.filter(item =>
           item.ORIGIN === this.origin &&
           item.DESTINATION === this.destination &&
           item.VOLUME === this.volume
         );
+        this.results = filtered;
+        this.searchResults.emit(filtered); // Emit results to parent
       });
   }
 }
